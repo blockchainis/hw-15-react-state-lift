e5ff811c28d22509dae8bbe737ebc694466396c8
hoon2 <sanghoon.engineer@gmail.com>
2022-10-11 07:34:22 +0900

    추가 설명코드

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,15 @@
+/**
+ * 입력시 숫자가 아닐경우 아래처럼 처리해주세요~
+ *if (isNaN(Number(e.target.value))) {
+    return;
+  }
+ */
+
 function Input({ name }) {
   return (
     <div>
       <label>
-        <input type="number" />
+        <input type="text" />
         {name}
       </label>
     </div>
